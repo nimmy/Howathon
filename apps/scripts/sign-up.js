8edb04ee15ef091cb155dc5f9f10b417248a1ee6
arunima <arunima.saxena@publicissapient.com>
2022-08-18 15:42:31 +0530

    otp page UI done

--- apps/scripts/sign-up.js
+++ apps/scripts/sign-up.js
@@ -1,10 +1,13 @@
 var getEl = document.querySelector('.sign-up-container');
 var getNextEl = document.querySelector('.user-details-wrapper');
+var getVerifyBtn = document.querySelector('.verify-btn');
 var togglePage = 'hide-page';
 
 function redirect(){
     getEl.classList.add(togglePage);    
     getNextEl.classList.remove(togglePage);
+    getVerifyBtn.classList.remove(togglePage);
+
 }
 
 function landingPage(){
